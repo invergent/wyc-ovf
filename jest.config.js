a7da75b9aa86d2eed93d065878d762b1b2d4762f
Invergent Technologies <invergenttechnologies@gmail.com>
2019-04-13 02:03:39 +0100

    chore(setup): setup angular project
    - create angular project using the cli
    - replace protractor and karma with cypress and jest respectively
    - modify configurations for jest and cypress
    
    [Finishes #165322144]

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  globals: {
+    "ts-jest": {
+      diagnostics: {
+        pathRegex: /\.(spec|test)\.ts$/
+      },
+      "allowSyntheticDefaultImports": true
+    }
+  }
+}
