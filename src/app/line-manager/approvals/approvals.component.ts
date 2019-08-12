394acb28493405b8d5abe20da5da012c719ff11a
Invergent Technologies <invergenttechnologies@gmail.com>
2019-08-13 00:57:15 +0100

    line manager can request edit/staff can update claim

--- src/app/line-manager/approvals/approvals.component.ts
+++ src/app/line-manager/approvals/approvals.component.ts
@@ -11,6 +11,18 @@ import {
   styleUrls: ['./approvals.component.scss']
 })
 export class ApprovalsComponent implements OnInit {
+  claimsToApprove: IClaimForLineManager[]
+  currentClaim: IClaimForLineManager
+  lineManager: ILineManager
+
+  displaySpinner: boolean = true;
+  displayThanks: string = 'none';
+  thanksModal: boolean = false;
+  requestEditSpinner: boolean = false;
+  requestEditModal: boolean = false;
+  message: string = '';
+
+  staffName: string
 
   constructor(
     private router: Router,
@@ -20,11 +32,6 @@ export class ApprovalsComponent implements OnInit {
     @Inject(JQUERY_TOKEN) private jQuery
   ) { }
 
-  displaySpinner: boolean = true;
-  displayThanks: string = 'none';
-  message: string = '';
-  lineManager: ILineManager
-  claimsToApprove: IClaimForLineManager[]
 
   async ngOnInit() {
     try {
@@ -34,7 +41,8 @@ export class ApprovalsComponent implements OnInit {
       this.message = 'the following claim requests await your approval';
       this.displaySpinner = false;
     } catch (error) {
-      if (error.status === 404) {this.lineManager = error.error.data.lineManager;
+      if (error.status === 404) {
+        this.lineManager = error.error.data.lineManager;
         this.claimsToApprove = error.error.data.pendingClaims;
         this.message = 'you currently have no pending claims to approve.';
         this.displaySpinner = false;
@@ -44,7 +52,8 @@ export class ApprovalsComponent implements OnInit {
     }
   }
 
-  async runApproval(approvalData) {
+  async runApproval(event, approvalData) {
+    event.preventDefault();
     const [approvalType, claimId] = approvalData.split(' ');
     const response = await this.lineManagerService.runApproval(approvalType, claimId);
     this.toaster.success(response.message);
@@ -52,9 +61,10 @@ export class ApprovalsComponent implements OnInit {
   }
 
   fadeThenRemoveApprovedClaim(approvalType, claimId) {
-    this.jQuery(`#${claimId}`)
+    const fadeColors = { approve: '#40ad4890', decline: '#84120B70', requestEdit: '#ec1e8d' }
+    this.jQuery(`#claim${claimId}`)
       .css('opacity', '0')
-      .css('background-color', approvalType === 'approve' ? '#40ad4890' : '#84120B70');
+      .css('background-color', fadeColors[approvalType]);
     setTimeout(() => {
       this.removeApprovedClaimFromList(claimId);
     }, 1000);
@@ -65,10 +75,48 @@ export class ApprovalsComponent implements OnInit {
     if (!(claimIndex === -1)) this.claimsToApprove.splice(claimIndex, 1);
     if (this.claimsToApprove.length === 0) {
       setTimeout(() => {
-        this.displayThanks = 'block';
+        this.toggleModalDisplay('thanksModal');
         this.message = 'you currently have no pending claims to approve.';
         this.notificationService.playAudio('../../../assets/audio/tada.mp3');
       }, 300);
+    }
+  }
+
+  validateRequestEditInput(editRequestMessage) {
+    if (!editRequestMessage) return ['Kindly state what needs to be edited.'];
+    if (editRequestMessage.length < 10) return ['Message too short'];
+    return [];
+  }
+
+  toggleModalDisplay(modal: string, currentClaim?: IClaimForLineManager, event?) {
+    if(event) event.preventDefault();
+    this[modal] = !this[modal];
+    if (this[modal]) {
+      this.displayThanks = 'block';
+      if (modal === 'requestEditModal') this.currentClaim = currentClaim;
+    } else {
+      this.displayThanks = 'none';
+      this.currentClaim = null;
+    }
+  }
+
+  async sendRequestEdit(editMessage) {
+    this.requestEditSpinner = true;
+    const errors = this.validateRequestEditInput(editMessage);
+    if (errors.length) {
+      this.requestEditSpinner = false;
+      return errors.forEach(error => this.toaster.error(error));
+    } 
+
+    try {
+      const { message } = await this.lineManagerService.requestEdit({ editMessage }, this.currentClaim.id);
+      this.requestEditSpinner = false;
+      this.toaster.success(message);
+      this.fadeThenRemoveApprovedClaim('requestEdit', this.currentClaim.id);
+      this.toggleModalDisplay('requestEditModal');
+    } catch (error) {
+      this.toaster.error('There was an error requesting edit on this claim.');
+      this.requestEditSpinner = false;
     }
   }
 }
