633629a9675e790bbccee38331fda12d96b7eee5
Invergent Technologies <invergenttechnologies@gmail.com>
2019-10-08 09:05:14 +0100

    admin can resend staff login credentials

--- src/app/admin/view-staff/view-staff.component.spec.ts
+++ src/app/admin/view-staff/view-staff.component.spec.ts
@@ -1,14 +1,20 @@
 import { ViewStaffComponent } from './view-staff.component';
-import { activatedRouteMock, profileServiceMock } from 'src/app/__mocks__';
+import { activatedRouteMock, profileServiceMock, mockToastr } from 'src/app/__mocks__';
 
 describe('ClaimEngineComponent', () => {
   let component: ViewStaffComponent;
 
   beforeEach(() => {
-    component = new ViewStaffComponent(activatedRouteMock, profileServiceMock);
+    component = new ViewStaffComponent(activatedRouteMock, profileServiceMock, mockToastr);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should resend activation email to selected staff', async () => {
+    const resendLoginMock = jest.spyOn(profileServiceMock, 'resendLoginCredentials');
+    const toastrSucMock = jest.spyOn(mockToastr, 'success');
+
+    await component.resendLoginCredentials('someStaffId');
+
+    expect(resendLoginMock).toHaveBeenCalledWith('someStaffId');
+    expect(toastrSucMock).toHaveBeenCalledWith('message');
   });
 });
