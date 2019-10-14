cb8a0c5b83362bba131853cffa69e9a72f031ea1
Invergent Technologies <invergenttechnologies@gmail.com>
2019-10-14 23:49:24 +0100

    setup for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://overtime-api.invergent-technologies.com',
+  api: 'https://cleontime-api.whytecleon.ng',
   API_KEY: 'b79fda95698b77ecf0a3'
 };
