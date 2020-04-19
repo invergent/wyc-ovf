0e7f49cbda9bb85a4301910f82e72ffcd875e829
Invergent Technologies <invergenttechnologies@gmail.com>
2020-04-19 18:36:27 +0100

    redeploy to Hexigin GCP

--- src/environments/environment.cloud_test.ts
+++ src/environments/environment.cloud_test.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://cleontime-api.whytecleon.ng',
+  api: 'https://cleontime-api-test.whytecleon.ng',
   API_KEY: 'b79fda95698b77ecf0a3'
 };
