0b20a4bf3a974a339b1c2e315682ebf28d23f63c
Invergent Technologies <invergenttechnologies@gmail.com>
2020-09-14 12:51:37 +0100

    Upgrade TLS to 1.2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const https = require("https");
 const path = require('path');
 
 const app = express();
@@ -69,4 +70,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/overtime-frontend/index.html'));
 });
 
-app.listen(port);
+const server = https.createServer({ minVersion: 'TLSv1.2' }, app)
+
+server.listen(port);
