bcc5338bb987c358a3d4ec32362a2d93063622bb
Invergent Technologies <invergenttechnologies@gmail.com>
2021-02-18 18:00:29 +0100

    Connect to appraisal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   api: 'https://cleontime-api.whytecleon.ng',
   API_KEY: 'b79fda95698b77ecf0a3',
-  appraisalUrl: 'https://appraisal.apis.whytecleon.ng/verify'
+  appraisalUrl: 'https://appraisal.whytecleon.ng/verify'
 };
